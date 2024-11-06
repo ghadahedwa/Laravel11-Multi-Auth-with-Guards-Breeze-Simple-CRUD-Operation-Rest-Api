f0c3038a899046c95ed0174dba318744a503cf7c
Ghada Mohammed <ghadahedwa.gmail.com>
2024-11-06 21:07:03 +0200

    sanctum auth api token

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
 
-Route::apiResource('products',ProductController::class);
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/register', [AuthController::class,'register']);
+
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::post('/logout', [AuthController::class,'logout']);
+    Route::get('/profile', [AuthController::class,'profile']);
+    Route::apiResource('products',ProductController::class);
+});
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
